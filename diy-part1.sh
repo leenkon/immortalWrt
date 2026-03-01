c467a1f5640c25746a56286a60e1ea6222eea024
leenkon <leenkon@qq.com>
2026-03-01 10:49:55 +0800

    gengx

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git small-package https://github.com/kenzok8/small-package.git' >>feeds.conf.default
