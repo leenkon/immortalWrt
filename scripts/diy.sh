7b88648d7646afb31ea1639293a9853b1b765efc
Leenkon <leenkon@qq.com>
2026-06-10 16:38:43 +0800

    更正配置逻辑

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -36,10 +36,10 @@ PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
 case "$PHASE" in
     before)
-        rm -f feeds.conf
-        [[ -f "$PROJECT_ROOT/feeds/$VERSION.conf" ]] && cp "$PROJECT_ROOT/feeds/$VERSION.conf" feeds.conf.default
+        rm -f feeds.conf feeds.conf.default
+        [[ -f "$PROJECT_ROOT/feeds/$VERSION.conf" ]] && cp "$PROJECT_ROOT/feeds/$VERSION.conf" feeds.conf
         ./scripts/feeds update -a
-        grep -qs 'src-git small' feeds.conf.default && {
+        grep -qs 'src-git small' feeds.conf && {
             rm -rf feeds/luci/applications/luci-app-mosdns feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat 2>/dev/null
             rm -rf feeds/packages/lang/golang && git clone --depth 1 -b 1.26 https://github.com/kenzok8/golang feeds/packages/lang/golang 2>/dev/null
         }
