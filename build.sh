6e501c17b67ab99c58c824d7f780f63c93d996f7
Leenkon <leenkon@qq.com>
2026-06-17 19:18:49 +0800

    优化脚本，尝试解决增加配置换行问题

--- build.sh
+++ build.sh
@@ -125,7 +125,7 @@ fi
 ./scripts/feeds install -a
 cp "$SCRIPT_DIR/configs/${MAIN_VER}-${CFG_PREFIX}.config" .config || error_exit "配置文件不存在"
 sed -i 's/\r$//' .config
-[[ "$USE_OAF" == "true" ]] && { echo "" >> .config; echo "CONFIG_PACKAGE_luci-app-oaf=y" >> .config; }
+[[ "$USE_OAF" == "true" ]] && echo -e "\nCONFIG_PACKAGE_luci-app-oaf=y" >> .config
 success "完成"
 
 # 5. 网络配置
